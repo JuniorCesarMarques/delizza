0eea7a7e7a05170de5d29c747ffb3ee40c7c5c02
JuniorCesarMarques <jcesargithub@gmail.com>
2025-08-26 19:48:53 -0300

    navbar and footer done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <div className="min-h-[80vh]">{children}</div>
+        <Navbar />
       </body>
     </html>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { CiHome } from "react-icons/ci";
+import { LuNotebookPen } from "react-icons/lu";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+export default function Navbar(){
+    return (
+        <nav>
+            <ul className="flex items-center justify-between px-10 bg-zinc-200 py-3">
+                <li className="flex flex-col items-center">
+                    <span><CiHome /></span>
+                    <span>Início</span>
+                </li>
+                <li className="flex flex-col items-center">
+                    <span><LuNotebookPen /></span>
+                    <span>Pedidos</span>
+                </li>
+                <li className="flex flex-col items-center">
+                    <span><AiOutlineShoppingCart /></span>
+                    <span>Carrinho</span>
+                </li>
+            </ul>
+        </nav>
+    )
+}
