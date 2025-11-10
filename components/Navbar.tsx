f80c36707defc67cfcf8219d94eb84ed0b56ba7d
JuniorCesarMarques <jcesargithub@gmail.com>
2025-11-09 21:16:04 -0300

    navegação para criação de produtos e categorias implementada provisoriamente

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,21 @@
+"use client"
+
 import { CiHome } from "react-icons/ci";
 import { LuNotebookPen } from "react-icons/lu";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Navbar(){
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     return (
         <nav className="fixed bottom-0 w-screen ">
             <ul className="flex items-center justify-between px-10 bg-zinc-200 py-3">
-                <li className="flex flex-col items-center">
+                <li onClick={() => setIsOpen(prev => !prev)} className="flex flex-col items-center">
                     <span><CiHome /></span>
-                    <span>Início</span>
+                    <span>Inicio</span>
                 </li>
                 <li className="flex flex-col items-center">
                     <span><LuNotebookPen /></span>
@@ -18,6 +25,14 @@ export default function Navbar(){
                     <span><AiOutlineShoppingCart /></span>
                     <span>Carrinho</span>
                 </li>
+            </ul>
+            <ul className={`${!isOpen && "hidden"} absolute bottom-15 left-0 flex flex-col gap-2`}>
+                <Link href="/category/new">
+                    <li>Criar categoria</li>
+                </Link>
+                <Link href="/product/new">
+                    <li>Cria produto</li>
+                </Link>
             </ul>
         </nav>
     )
